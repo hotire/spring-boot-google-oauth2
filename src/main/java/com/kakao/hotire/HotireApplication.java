bf984be261c5b871c71d6913e655717c80ba36ba
user <user>
2018-07-28 15:03:11 +0900

    init Quickstart

---
+++ src/main/java/com/kakao/hotire/HotireApplication.java
@@ -0,0 +1,12 @@
+package com.kakao.hotire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotireApplication.class, args);
+    }
+}
---
+++ src/main/java/com/kakao/hotire/contoller/MainController.java
@@ -0,0 +1,8 @@
+package com.kakao.hotire.contoller;
+
+/**
+ * @author : hotire
+ */
+public class MainController {
+    // https://github.com/hoTire/spring-boot-oauth2.git
+}
